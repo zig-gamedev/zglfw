12437843f20c405b36f0f17fb45d6b10b06243d4
Connor Rowland <connor.k.rowland@icloud.com>
2024-11-18 10:47:46 -0800

    Initial commit

--- src/zglfw.zig
+++ src/zglfw.zig
@@ -1107,3 +1107,26 @@ fn _isLinuxDesktopLike() bool {
         else => false,
     };
 }
+
+//--------------------------------------------------------------------------------------------------
+//
+// Emscripten
+//
+//--------------------------------------------------------------------------------------------------
+const os = builtin.target.os.tag;
+usingnamespace if (os == .emscripten or os == .freestanding) struct {
+    // GLFW - emscripten uses older version that doesn't have these functions - implement dummies
+    var glfwGetGamepadStateWarnPrinted: bool = false;
+    pub export fn glfwGetGamepadState(_: i32, _: ?*anyopaque) i32 {
+        if (!glfwGetGamepadStateWarnPrinted) {
+            std.log.err("glfwGetGamepadState(): not implemented! Use emscripten specific functions: https://emscripten.org/docs/api_reference/html5.h.html?highlight=gamepadstate#c.emscripten_get_gamepad_status", .{});
+            glfwGetGamepadStateWarnPrinted = true;
+        }
+        return 0; // false - failure
+    }
+
+    /// use glfwSetCallback instead
+    pub export fn glfwGetError() i32 {
+        return 0; // no error
+    }
+} else struct {};
